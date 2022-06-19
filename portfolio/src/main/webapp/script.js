2050a79695c46a707d3fa07fde5fa559f38b5a88
Garyouki <z.zihaogary@wustl.edu>
2022-06-19 23:39:55 +0000

    datastore

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -75,6 +75,26 @@ async function getServerStats() {
         document.getElementById('map'),
         {center: {lat: 37.422, lng: -122.084}, zoom: 16});
   }
+
+  function loadTasks() {
+    fetch('/list-tasks').then(response => response.json()).then((tasks) => {
+      const taskListElement = document.getElementById('task-list');
+      tasks.forEach((task) => {
+        taskListElement.appendChild(createTaskElement(task));
+      })
+    });
+  }
+  
+  /** Creates an element that represents a task, including its delete button. */
+  function createTaskElement(task) {
+    const taskElement = document.createElement('li');
+    taskElement.className = 'task';
+  
+    const titleElement = document.createElement('span');
+    titleElement.innerText = task.title;
+    taskElement.appendChild(titleElement);
+    return titleElement;
+  }
   
   
   

db57466c345860b39d35c3977b2eea6d7814a93a
Garyouki <z.zihaogary@wustl.edu>
2022-06-19 04:52:31 +0000

    map

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -70,6 +70,11 @@ async function getServerStats() {
     });
   }
   
- 
+  function createMap() {
+    const map = new google.maps.Map(
+        document.getElementById('map'),
+        {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+  }
+  
   
   
